ef57e69ddfeb3727ea7eed8a95b83c861920095b
Morten Bøgh <mob@trifork.com>
2014-07-21 21:48:10 +0200

    Initial Commit

---
+++ Xcode Live Rendering/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode Live Rendering
+//
+//  Created by Morten Bøgh on 21/07/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
