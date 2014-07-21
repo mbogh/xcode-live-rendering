20f7af88ed91f2a3c30decf82b2ad9902ebc43f6
Morten Bøgh <mob@trifork.com>
2014-07-21 22:11:16 +0200

    Added the framework for the views.

---
+++ Xcode Live Rendering Views/Xcode Live Rendering Views.h
@@ -0,0 +1,19 @@
+//
+//  Xcode Live Rendering Views.h
+//  Xcode Live Rendering Views
+//
+//  Created by Morten Bøgh on 21/07/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Xcode Live Rendering Views.
+FOUNDATION_EXPORT double Xcode_Live_Rendering_ViewsVersionNumber;
+
+//! Project version string for Xcode Live Rendering Views.
+FOUNDATION_EXPORT const unsigned char Xcode_Live_Rendering_ViewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Xcode_Live_Rendering_Views/PublicHeader.h>
+
+
