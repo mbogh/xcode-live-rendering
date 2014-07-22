77eebe22d894b34fbdc8d8446aa1b3c2ded084b9
Morten Bøgh <mob@trifork.com>
2014-07-22 13:33:43 +0200

    Added two inspectable properties.

--- Xcode Live Rendering Views/LiveView.swift
+++ Xcode Live Rendering Views/LiveView.swift
@@ -10,6 +10,34 @@ import UIKit
 
 @IBDesignable
 class LiveView: UIView {
+    private var titleLabel = UILabel()
+    private var avatarImageView = UIImageView()
+    
+    @IBInspectable public var title: String = "" {
+        didSet {
+            self.titleLabel.text = title
+        }
+    }
+    
+    @IBInspectable public var avatarImage: UIImage = UIImage() {
+        didSet {
+            self.avatarImageView.image = avatarImage
+        }
+    }
+    
+    init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+        self.commonInit()
+    }
+    
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        self.commonInit()
+    }
+    
+    private func commonInit() {
+    }
+    
     override func prepareForInterfaceBuilder() {
     }
 }
