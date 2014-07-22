833be16363d54699e42aa5b366b4830eddd5e0f1
Morten Bøgh <mob@trifork.com>
2014-07-22 08:57:36 +0200

    Added LiveView in the framework.

---
+++ Xcode Live Rendering Views/LiveView.swift
@@ -0,0 +1,15 @@
+//
+//  LiveView.swift
+//  Xcode Live Rendering
+//
+//  Created by Morten Bøgh on 21/07/14.
+//  Copyright (c) 2014 Morten Bøgh. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LiveView: UIView {
+    override func prepareForInterfaceBuilder() {
+    }
+}
